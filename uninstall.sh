a5e186554173b48ea47ea4e8175e2324ec15403e
Michael Edgar <medgar@redhat.com>
2021-06-04 09:41:07 -0400

    Refine deployment and uninstall processes
    - Change observability `success` check to `in progress` check
    - Force success for uninstall scripts, to enhance in next phase(es)
    
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- uninstall.sh
+++ uninstall.sh
@@ -22,16 +22,27 @@ DIR_NAME="$(dirname $0)"
 
 KAS_INSTALLER_ENV_FILE="kas-installer.env"
 
-(cd operators && ./uninstall-kas-fleetshard.sh && ./uninstall-strimzi-cluster-operator.sh)
+(cd operators && \
+  ./uninstall-kas-fleetshard.sh && \
+  ./uninstall-strimzi-cluster-operator.sh)
 
-source kas-fleet-manager/kas-fleet-manager-deploy.env
+KAS_FLEET_MANAGER_DIR="kas-fleet-manager"
+KAS_FLEET_MANAGER_DEPLOY_ENV_FILE="${KAS_FLEET_MANAGER_DIR}/kas-fleet-manager-deploy.env"
+TERRAFORM_FILES_BASE_DIR="terraforming"
+TERRAFORM_GENERATED_DIR="${KAS_FLEET_MANAGER_DIR}/${TERRAFORM_FILES_BASE_DIR}/terraforming-generated-k8s-resources"
+source ${KAS_FLEET_MANAGER_DEPLOY_ENV_FILE}
 
-${KUBECTL} delete namespace ${KAS_FLEET_MANAGER_NAMESPACE}
+${KUBECTL} delete observabilities --all -n managed-application-services-observability || true
 
-${KUBECTL} delete observabilities --all -n managed-application-services-observability
-${KUBECTL} delete namespace managed-application-services-observability
+for i in $(find ${TERRAFORM_GENERATED_DIR} -type f | sort); do
+    echo "Deleting K8s resource ${i} ..."
+    ${KUBECTL} delete -f ${i} || true
+done
 
-${KUBECTL} delete keycloakclients -l app=mas-sso --all-namespaces
-${KUBECTL} delete keycloakrealms -n mas-sso
-${KUBECTL} delete keycloaks -l app=mas-sso --all-namespaces
-${KUBECTL} delete namespace mas-sso
+${KUBECTL} delete namespace ${KAS_FLEET_MANAGER_NAMESPACE} || true
+${KUBECTL} delete namespace managed-application-services-observability || true
+
+${KUBECTL} delete keycloakclients -l app=mas-sso --all-namespaces || true
+${KUBECTL} delete keycloakrealms --all -n mas-sso || true
+${KUBECTL} delete keycloaks -l app=mas-sso --all-namespaces || true
+${KUBECTL} delete namespace mas-sso || true

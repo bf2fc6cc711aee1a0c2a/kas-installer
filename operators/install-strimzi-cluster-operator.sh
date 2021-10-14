54c52e622c51943de1c590307cbc4022d27a6d14
Ramesh Reddy <rareddy@jboss.org>
2021-10-14 12:04:47 -0500

    removing the creation of tmp directory and doing the token replacement inline

--- operators/install-strimzi-cluster-operator.sh
+++ operators/install-strimzi-cluster-operator.sh
@@ -8,33 +8,19 @@ KUBECTL=$(which kubectl)
 if [ "$OS" = 'Darwin' ]; then
   # for MacOS
   SED=$(which gsed)
-  CP=$(which gcp)
 else
   # for Linux and Windows
   SED=$(which sed)
-  CP=$(which cp)
 fi
-
-if ! [ -d strimzi-cluster-operator/resources/tmp ]; then
-    mkdir strimzi-cluster-operator/resources/tmp
-fi
-
-rm -rf strimzi-cluster-operator/resources/tmp/*
-
-${CP} -t strimzi-cluster-operator/resources/tmp/ strimzi-cluster-operator/resources/*.yaml
-
-${SED} -i "s/namespace: .*/namespace: ${NAMESPACE}/" \
-    strimzi-cluster-operator/resources/tmp/*.yaml
 
 # Create the namespace if it's not found
 ${KUBECTL} get ns ${NAMESPACE} >/dev/null \
   || ${KUBECTL} create ns ${NAMESPACE}
 
-${SED} -i "s/image: .*/image: ${BUNDLE_IMAGE}/" \
-    strimzi-cluster-operator/resources/tmp/*.yaml
+CATALOG_SOURCE_YAML=$(< strimzi-cluster-operator/kas-catalog-source.yaml)
 
-${KUBECTL} create -f strimzi-cluster-operator/resources/tmp/kas-catalog-source.yaml -n ${NAMESPACE}
-${KUBECTL} create -f strimzi-cluster-operator/resources/tmp/kas-strimzi-opgroup.yaml -n ${NAMESPACE}
-${KUBECTL} create -f strimzi-cluster-operator/resources/tmp/kas-strimzi-subscription.yaml -n ${NAMESPACE}
+echo "$CATALOG_SOURCE_YAML" | sed "s/image: .*/image: ${BUNDLE_IMAGE}/" | ${KUBECTL} create -n ${NAMESPACE} -f -
+${KUBECTL} create -f strimzi-cluster-operator/kas-strimzi-opgroup.yaml -n ${NAMESPACE}
+${KUBECTL} create -f strimzi-cluster-operator/kas-strimzi-subscription.yaml -n ${NAMESPACE}
 
 exit ${?}

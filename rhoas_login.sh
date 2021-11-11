a6529aca340533219681968072d7054fb02d8cd5
Michael Edgar <medgar@redhat.com>
2021-11-11 14:52:11 -0500

    Use insecure option and provide mas-auth-url for rhoas login
    Signed-off-by: Michael Edgar <medgar@redhat.com>

--- rhoas_login.sh
+++ rhoas_login.sh
@@ -5,5 +5,8 @@ source ${DIR_NAME}/kas-installer.env
 
 echo "Login with ${RH_USERNAME}/${RH_USERNAME}"
 
-rhoas login --auth-url $(oc get route -n mas-sso keycloak -o json | jq -r '"https://"+.spec.host+"/auth/realms/rhoas"')  --api-gateway $(oc get route -n kas-fleet-manager-${USER} kas-fleet-manager -o json | jq -r '"https://"+.spec.host')
-
+rhoas login \
+ --insecure \
+ --auth-url $(oc get route -n mas-sso keycloak -o json | jq -r '"https://"+.spec.host+"/auth/realms/rhoas"')  \
+ --mas-auth-url $(oc get route -n mas-sso keycloak -o json | jq -r '"https://"+.spec.host+"/auth/realms/rhoas"')  \
+ --api-gateway $(oc get route -n kas-fleet-manager-${USER} kas-fleet-manager -o json | jq -r '"https://"+.spec.host')

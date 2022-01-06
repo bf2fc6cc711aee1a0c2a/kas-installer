cdc17db6e3acb2401ff3060c6d2e4455e6af7f8e
Ramesh Reddy <rareddy@jboss.org>
2022-01-06 12:07:05 -0600

    restoring the kas-fleetshard-operator-restarter.yml script for cron job which is not part of the bundle

--- operators/uninstall-kas-fleetshard.sh
+++ operators/uninstall-kas-fleetshard.sh
@@ -10,6 +10,7 @@ ${KUBECTL} delete subscription kas-fleeshard-subscription -n ${NAMESPACE}
 ${KUBECTL} delete csv ${CSV} -n ${NAMESPACE}
 ${KUBECTL} delete operatorgroup kas-fleeshard-bundle -n ${NAMESPACE}
 ${KUBECTL} delete catalogsource kas-fleeshard-catalog -n ${NAMESPACE}
+${KUBECTL} delete -f kas-fleetshard/resources -n ${NAMESPACE}
 
 # remove all CRDs
 for c in $(${KUBECTL} get crd -l operators.coreos.com/kas-fleetshard-operator.redhat-kas-fleetshard-operator='' --no-headers | cut -d " " -f1); do

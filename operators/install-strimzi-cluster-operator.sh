f15aece473b96132aa8e3be67f6e490d61f26dc7
Michael Edgar <medgar@redhat.com>
2022-02-09 12:29:16 -0500

    Bump default rhosak-index to 0.0.10-1

--- operators/install-strimzi-cluster-operator.sh
+++ operators/install-strimzi-cluster-operator.sh
@@ -2,7 +2,7 @@
 
 OS=$(uname)
 NAMESPACE=${STRIMZI_OPERATOR_NAMESPACE:-redhat-managed-kafka-operator}
-BUNDLE_IMAGE=${STRIMZI_OPERATOR_BUNDLE_IMAGE:-quay.io/osd-addons/rhosak-index@sha256:14a1831f4fee851ce3e22b4817813ca43868c6a30769a4fda58a1e2484558ec1}
+BUNDLE_IMAGE=${STRIMZI_OPERATOR_BUNDLE_IMAGE:-quay.io/osd-addons/rhosak-index@sha256:5dc60972abcec63bc2b584123f950cb6869bcb8718b6d12a9832de86b07ac699}
 KUBECTL=$(which kubectl)
 OC=$(which oc)
 

94af9545a73def6eb0d0e23e036ba07ec5ca4437
Keith Wall <kwall@apache.org>
2022-03-11 19:25:58 +0000

    Update again

--- operators/install-strimzi-cluster-operator.sh
+++ operators/install-strimzi-cluster-operator.sh
@@ -2,7 +2,7 @@
 
 OS=$(uname)
 NAMESPACE=${STRIMZI_OPERATOR_NAMESPACE:-redhat-managed-kafka-operator}
-BUNDLE_IMAGE=${STRIMZI_OPERATOR_BUNDLE_IMAGE:-quay.io/osd-addons/rhosak-index@sha256:10c097987fe1691a197ba0cb8f6694859b1f2ead81330c190a5bb0cf45683048}
+BUNDLE_IMAGE=${STRIMZI_OPERATOR_BUNDLE_IMAGE:-quay.io/osd-addons/rhosak-index@sha256:94e285cdbcd178e1d9a41fae9999c6607810856b0ae3dcbaf871decf1879c7ab}
 KUBECTL=$(which kubectl)
 OC=$(which oc)
 

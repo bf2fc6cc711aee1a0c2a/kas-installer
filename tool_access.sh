2c6932ab22e98c39333d8b6cf24aedf5a7f62e5d
Steven Hawkins <shawkins@redhat.com>
2022-08-03 15:53:28 -0400

    Update tool_access.sh
    Co-authored-by: Michael Edgar <michael@xlate.io>

--- tool_access.sh
+++ tool_access.sh
@@ -12,9 +12,9 @@ ADMIN_API_SERVER_URL=`echo ${CLUSTER} | jq -r '.admin_api_server_url'`
 
 TOKEN=`./get_access_token.sh --owner`
 
-curl -vs   -H"Authorization: Bearer ${TOKEN}   ${ADMIN_API_SERVER_URL}/api/v1/acls   -XPOST   -H'Content-type: application/json'   --data '{"resourceType":"GROUP", "resourceName":"*", "patternType":"LITERAL", "principal":"User:'${SERVICE_ACCT}'", "operation":"ALL", "permission":"ALLOW"}'
+curl -vs   -H"Authorization: Bearer ${TOKEN}" ${ADMIN_API_SERVER_URL}/api/v1/acls   -XPOST   -H'Content-type: application/json'   --data '{"resourceType":"GROUP", "resourceName":"*", "patternType":"LITERAL", "principal":"User:'${SERVICE_ACCT}'", "operation":"ALL", "permission":"ALLOW"}'
 
-curl -vs   -H"Authorization: Bearer ${TOKEN}   ${ADMIN_API_SERVER_URL}/api/v1/acls   -XPOST   -H'Content-type: application/json'   --data '{"resourceType":"TOPIC", "resourceName":"*", "patternType":"LITERAL", "principal":"User:'${SERVICE_ACCT}'", "operation":"ALL", "permission":"ALLOW"}'
+curl -vs   -H"Authorization: Bearer ${TOKEN}" ${ADMIN_API_SERVER_URL}/api/v1/acls   -XPOST   -H'Content-type: application/json'   --data '{"resourceType":"TOPIC", "resourceName":"*", "patternType":"LITERAL", "principal":"User:'${SERVICE_ACCT}'", "operation":"ALL", "permission":"ALLOW"}'
 
 echo 'Bootstrap Server Host:'
 echo ${CLUSTER} | jq -r '.bootstrap_server_host'
